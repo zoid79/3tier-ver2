bae9d1de97b528cc691dc99996097c22a3f9d2ae
zoid79 <zidwkd00@naver.com>
2024-06-26 09:50:45 +0900

    update App.js

--- step2/client/src/App.js
+++ step2/client/src/App.js
@@ -31,7 +31,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>확신없는 랜덤 명언</h1>
+      <h1>확신있는 랜덤 명언</h1>
       <h2>{displayedText ? displayedText : "아직 저장된 명언이 없거나 서버와 연결되지 않았습니다."}</h2>
       <h2>by {displayedAuthor}</h2>
       <form onSubmit={handleSubmit}>
